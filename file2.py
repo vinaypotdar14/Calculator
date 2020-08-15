320c87c65c44541653d8588be64a6fea97031411
vinaypotdar14 <69380851+vinaypotdar14@users.noreply.github.com>
2020-08-15 15:19:53 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,14 @@
+num1=int(input("enter your first number:   "))
+num2=int(input("enter your second number:  "))
+operator=input("tell operator:  ")
+
+if  operator=="+":
+   print( num1+num2)
+elif  operator== "-":
+    print(num1-num2)
+elif operator== "*":
+   print(num1*num2)
+elif operator== "/":
+    print(num1/num2)
+
+#print("sum of your two numbers is", sum)
